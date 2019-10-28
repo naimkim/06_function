cf0a3a74ebaaaf7de4e96f05db8f08282e48343e
naimkim <naimkim2000@naver.com>
2019-10-29 00:44:25 +0900

    04_project

---
+++ main(again).c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+		int result;
+	result=square(5);
+	printf("%d",result);
+	
+	return 0;
+}
+int square(int n)
+{
+	return(n*n);
+}
+
