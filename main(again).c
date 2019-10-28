a096319666d94374dc7724034a8045550c792d28
naimkim <naimkim2000@naver.com>
2019-10-29 00:51:45 +0900

    06_project

--- main(again).c
+++ main(again).c
@@ -4,20 +4,15 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int sum;
-	sum=compute_sum(100);
-	printf("sum=%d\n",sum);
 	
+	int i;
+	int max=45;
+	
+	srand((unsigned)time(NULL));
+	for(i=0;i<6;i++)
+	printf("%d ", 1+rand()%max);
+
+	return 0;
 }
-	
-	int compute_sum(int n)
-	{
-		int i;
-		int result=0;
-		for(i=1;i<=n;i++)
-			result +=i;
-			
-		return result;
-	}
 
 
